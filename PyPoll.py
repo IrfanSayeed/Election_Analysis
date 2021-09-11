70522befbb8183e81e5f69fec1f2b96cfe150388
DanielleSpring <daniellemulvihill@hotmail.com>
2021-09-11 10:44:33 -0400

    2nd commit
    test comment

--- PyPoll.py
+++ PyPoll.py
@@ -4,6 +4,7 @@
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
+#6. Adding a test comment for demo
 
 import csv
 import os
