0d11e97ef6cd6aa24d9246f167e7c93526d491fe
DanielleSpring <daniellemulvihill@hotmail.com>
2021-09-11 10:40:21 -0400

    1st commit
    Adding adding homework

---
+++ PyPoll.py
@@ -0,0 +1,24 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who recieved votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
+
+import csv
+import os
+
+#Assign a variable for the file to load and the path.
+file_to_load="C:\Users\Danielle Spring\Documents\Big Data Course\Challenges\Election_Analysis\Resources\election_results.csv"
+#Open the election results and read the file.
+election_data=open(election_results.csv,"r")
+    #to do: perform analysis.
+    print(election_data)
+#close the file
+election_data.close()
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who recieved votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
